8d4533079d3c4329b6b46fc87b4f008fdb80adf0
Giandomenico Olini <giando.olini@gogodigital.it>
2016-03-05 20:02:36 +0100

    Implementing Pull Request #6

--- controllers/ItemsController.php
+++ controllers/ItemsController.php
@@ -7,7 +7,7 @@
 * @github https://github.com/cinghie/yii2-articles
 * @license GNU GENERAL PUBLIC LICENSE VERSION 3
 * @package yii2-articles
-* @version 0.6.1
+* @version 0.6.2
 */
 
 namespace cinghie\articles\controllers;
