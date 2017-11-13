9b3ca0dba6929a993331b3a05134e65fe19658a4
Giandomenico Olini <giando.olini@gogodigital.it>
2017-11-13 19:07:11 +0100

    Update Version

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -7,7 +7,7 @@
 * @github https://github.com/cinghie/yii2-articles
 * @license GNU GENERAL PUBLIC LICENSE VERSION 3
 * @package yii2-articles
-* @version 0.6.3
+* @version 0.6.4
 */
 
 namespace cinghie\articles\controllers;
