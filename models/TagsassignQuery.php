7baee947a2a1b10a26d54f7999a473c304cab78c
Giandomenico Olini <giando.olini@gogodigital.it>
2017-12-07 13:41:39 +0100

    Adding TagsassignQuery

---
+++ models/TagsassignQuery.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-articles
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-articles
+ * @version 0.6.4
+ */
+
+namespace cinghie\articles\models;
+
+/**
+ * This is the ActiveQuery class for [[Tagsassign]].
+ * @see Tagsassign
+ */
+class TagsassignQuery extends \yii\db\ActiveQuery
+{
+
+    /**
+     * @inheritdoc
+     *
+     * @return Tagsassign[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return Tagsassign|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+
+}
