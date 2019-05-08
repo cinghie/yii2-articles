e8bbbc2b89acfd92b2d5dccef8f413d23e48798f
Giando <giando.olini@gogodigital.it>
2019-05-08 20:46:03 +0200

    Update FrontendFilter

--- filters/FrontendFilter.php
+++ filters/FrontendFilter.php
@@ -4,10 +4,10 @@
  * @copyright Copyright &copy; Gogodigital Srls
  * @company Gogodigital Srls - Wide ICT Solutions
  * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-user-extended
+ * @github https://github.com/cinghie/yii2-articles
  * @license GNU GENERAL PUBLIC LICENSE VERSION 3
- * @package yii2-user-extended
- * @version 0.6.2
+ * @package yii2-articles
+ * @version 0.6.5
  */
 
 namespace cinghie\articles\filters;
